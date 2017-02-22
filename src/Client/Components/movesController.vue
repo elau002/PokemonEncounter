ae60b3819c5aadd7742c685e160fd8418c15eff1
Eric Lau <elau002@gmail.com>
2017-02-22 15:36:15 -0800

    Updated Show All Display

--- src/Client/Components/movesController.vue
+++ src/Client/Components/movesController.vue
@@ -7,8 +7,9 @@
     <div v-if="showAll">
       <div v-for='move in allMoves'>
           <div>
+            <p>{{ move.id }}</p>
             <p>{{ move.name[0].toUpperCase() + move.name.slice(1) }}</p>
-            <p>{{move.id}}</p>
+            <p>{{ move.type[0].toUpperCase() + move.type.slice(1) }}</p>
           </div>
         </div>
     </div>
@@ -25,8 +26,7 @@
        }  
      },
      created () {
-      // this.getAllMoves();
-      // this.getMove();
+      this.getAllMoves();
      },
      methods: {
        getAllMoves: function () {
@@ -40,25 +40,12 @@
         })
        },
        getMove: function (name) {
-               let temp = [];
-      for(let i = 359; i < 622; i++) {
-        temp.push(i);
-      }
-         Promise.all(temp.map((number, index)=> {
-          return new Promise((resolve, reject)=> {
-          this.$http.get('http://localhost:4824/api/move', {params: {id : number} } )
-            .then((res) => {
-              console.log(res.body);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-          }
-            );
-          }))
-          .then((value)=>{ 
-            this.$data.generations[gen] = value;
-            this.$data.pokemons = value;
+        this.$http.get('http://localhost:4824/api/move', {params: {id : number} } )
+          .then((res) => {
+            console.log(res.body);
+          })
+          .catch((err) => {
+            console.log(err);
           });
        },
      }
