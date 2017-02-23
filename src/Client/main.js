32c895a5601dabd3faea5aa7780b736223107414
Eric Lau <elau002@gmail.com>
2017-02-23 13:48:35 -0800

    Added mixin to main app

--- src/Client/main.js
+++ src/Client/main.js
@@ -3,11 +3,14 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 
 import router from './Router/router.js';
+import mixin from './Components/mixinComponent.js';
 
 // import App from './app.vue';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+
+Vue.mixin(mixin);
 
 
 const app = new Vue({
