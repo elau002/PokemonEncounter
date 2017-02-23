95273ec8c6d28cfb698767fb214b9641332f9ba7
Eric Lau <elau002@gmail.com>
2017-02-23 13:09:05 -0800

    Created request handler and server-side routes for types

---
+++ src/Server/handlers/typeHandler.js
@@ -0,0 +1,67 @@
+const Type = require('../../Database/models/typeModel.js');
+const db = require('../../Database/config.js');
+const rp = require('request-promise');
+const bodyParser = require('body-parser');
+
+const pkmnURL = 'http://pokeapi.co/api/v2/';
+
+exports.createTypeDB = (type, cb)=> {
+  Type.create(type, (err, type)=> {
+    if (err) { return cb(err); }
+    cb(null, type);
+  });
+};
+
+exports.getTypeDB = (type, cb)=> {
+  Type.findOne( { id: type } )
+  .exec((err, type) => {
+    if (err) { console.error(err); }
+    cb(null, type);
+  }); 
+};
+
+exports.getAllTypes = (req, res)=>{
+  Type.find()
+  .exec((err, type) => {
+    if (err) { console.error(err); }
+    res.send(type.sort((a, b)=> { return a.id - b.id; }));
+  });
+};
+
+exports.getOneType = (req, res)=> {
+  let name = req.query.id;
+  exports.getTypeDB(name, (err, type)=> {
+    if (err) { console.error(err); }
+    if (!type) {
+      exports.getTypeFromExternalAPI(name, res);
+    } else {
+      res.send(type);
+    }
+  });
+};
+
+exports.getTypeFromExternalAPI = (id, res)=> {
+  rp(pkmnURL + 'type/' + id + '/')
+    .then((response, next)=> {
+      response = JSON.parse(response);
+      let entry = {
+        id: response.id,
+        name: response.name,
+        damageRelations: response.damage_relations,
+        moves: response.moves.map((move)=> {
+          return move.name;
+        }),
+        pokemon: response.pokemon.map((pkmn)=> {
+          return pkmn.pokemon.name;
+        })
+      };
+      exports.createTypeDB(entry, (err, type)=>{
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(type);
+        }
+      });
+    })
+    .catch((err)=> { console.error(err); });
+};
--- src/Server/routes/apiRoute.js
+++ src/Server/routes/apiRoute.js
@@ -3,6 +3,8 @@ const pokemonHandler = require('../handlers/pokemonHandler.js');
 const trainerHandler = require('../handlers/trainerHandler.js');
 const moveHandler = require('../handlers/moveHandler.js');
 const abilityHandler = require('../handlers/abilityHandler.js');
+const typeHandler = require('../handlers/typeHandler.js');
+
 
 router.get('/pokemon', pokemonHandler.getOnePokemon);
 router.get('/pokemon/all', pokemonHandler.getAllPokemon);
@@ -14,5 +16,8 @@ router.get('/move/all', moveHandler.getAllMoves);
 
 router.get('/abilities', abilityHandler.getOneAbility);
 router.get('/abilities/all', abilityHandler.getAllAbilities);
+
+router.get('/type', typeHandler.getOneType);
+router.get('/type/all', typeHandler.getAllTypes);
 
 module.exports = router;
