a5713f4eaa93843e9a655be5498465ed24352d4a
Eric Lau <elau002@gmail.com>
2017-02-23 13:09:14 -0800

    Created type component

---
+++ src/Client/Components/typeController.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1> Types </h1>
+    <button v-if='!showAll' v-on:click.prevent="showAll = !showAll">Show All</button>
+    <button v-else v-on:click.prevent="showAll = !showAll">Hide All</button> 
+    <div v-if="showAll">
+      <div v-for='types in allTypes'>
+          <div>
+            <p>{{ types.name[0].toUpperCase() + types.name.slice(1) }}</p>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  module.exports = {
+    data: ()=> {
+      return {
+        allTypes: null,
+        showAll: false,
+      }
+    },
+    created () {
+      this.getAllTypes();
+    },
+    methods: {
+      getAllTypes: function () {
+        this.$http.get('http://localhost:4824/api/type/all')
+        .then((res)=> {
+          this.$data.allTypes = res.body;
+        })
+        .catch((err)=> {
+          console.log(err);
+        })
+       },
+      getTypes: function () {
+        this.$http.get('http://localhost:4824/api/type/', { params: { id: number}})
+          .then((res) => {
+            console.log(res.body);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+    }
+  }
+</script>
