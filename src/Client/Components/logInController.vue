73169f13e5a6228b75ce83e1de0dc9b69d818231
Eric Lau <elau002@gmail.com>
2017-02-23 18:19:38 -0800

    Created login component

---
+++ src/Client/Components/logInController.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div v-if='!loggedIn'>
+      <input v-model="username">
+      <input type='password' v-model="password">
+      <button v-on:click.prevent="logIn(username, password)">Log In</button>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: ()=> {
+    return  {
+      username: '',
+      password: '',
+      loggedIn: false,
+    }
+  },
+  methods: {
+    logIn: function (user, pass) {
+      let body = {
+        username: user,
+        password: pass
+        }
+      this.$http.get('http://localhost:4824/api/trainer/login', { params:  body })
+        .then((res)=> {
+          // this.$router.push('/database');
+        })
+        .catch((err)=> {
+          console.log(err);
+        })
+    }
+  },
+}
+</script>
