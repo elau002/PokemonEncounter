5e2d4910a59346249e0fedb8bfbc95a4556aaa35
Eric Lau <elau002@gmail.com>
2017-02-23 18:16:34 -0800

    Basic encounterController created

---
+++ src/Client/Components/encounterController.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Encounter</p>
+    <button v-on:click.prevent='getSingleQuery("pokemon", encounterRateGen())'>Roll</button>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: ()=> {
+    return  {
+      previous: [],
+      encounter: null,
+    }
+  },
+  methods: {
+    encounterRateGen: () =>{
+      return Math.floor(Math.random()* 721);
+    }
+  },
+}
+</script>
