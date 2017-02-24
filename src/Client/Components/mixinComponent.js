c9b11b9b959bc38c35e1ad41fcd3f38155fa1684
Eric Lau <elau002@gmail.com>
2017-02-24 14:03:16 -0800

    Added conditionals to set and check selected target

--- src/Client/Components/mixinComponent.js
+++ src/Client/Components/mixinComponent.js
@@ -1,9 +1,17 @@
 const mixinComponent = {
   methods: {
-    getSingleQuery: function (target, number) {
+    getSingleQuery: function (target, number, data) {
       this.$http.get(`http://localhost:4824/api/${target}/`, { params: { id: number } })
         .then((res) => {
-          console.log(res.body);
+          if (!data.infoSelect) {
+            data.infoSelect = res.body;
+            data.moreInfo = true;
+          } else if (data.infoSelect.name !== res.body.name) {
+            data.infoSelect = res.body;
+            data.moreInfo = true;
+          } else {
+            data.moreInfo = null;
+          }
         })
         .catch((err) => {
           console.log(err);
