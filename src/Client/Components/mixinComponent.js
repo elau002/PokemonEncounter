6c6a06e77ab87150c9c718ef676694b983209666
Eric Lau <elau002@gmail.com>
2017-02-28 13:29:54 -0800

    Formatted into ES6 and added query by name for pokemon

--- src/Client/Components/mixinComponent.js
+++ src/Client/Components/mixinComponent.js
@@ -1,16 +1,41 @@
 const mixinComponent = {
   methods: {
-    getSingleQuery: function (target, number, data) {
-      this.$http.get(`http://localhost:4824/api/${target}/`, { params: { id: number } })
+    getSingleQuery: (http, target, number, data)=> {
+      http.get(`http://localhost:4824/api/${target}/`, { params: { id: number } })
         .then((res) => {
-          if (!data.infoSelect) {
-            data.infoSelect = res.body;
-            data.moreInfo = true;
-          } else if (data.infoSelect.name !== res.body.name) {
-            data.infoSelect = res.body;
-            data.moreInfo = true;
-          } else {
-            data.moreInfo = null;
+          if (data) {
+            if (!data.infoSelect) {
+              res.body.query = target;
+              data.infoSelect = res.body;
+              data.moreInfo = true;
+            } else if (data.infoSelect.name !== res.body.name) {
+              res.body.query = target;
+              data.infoSelect = res.body;
+              data.moreInfo = true;
+            } else {
+              data.moreInfo = null;
+            }
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    getSinglePokemonByName: (http, name, data)=> {
+      http.get('http://localhost:4824/api/pokemon/name', { params: { name: name } })
+        .then((res) => {
+          if (data) {
+            if (!data.infoSelect) {
+              res.body.query = 'pokemon';
+              data.infoSelect = res.body;
+              data.moreInfo = true;
+            } else if (data.infoSelect.name !== res.body.name) {
+              res.body.query = 'pokemon';
+              data.infoSelect = res.body;
+              data.moreInfo = true;
+            } else {
+              data.moreInfo = null;
+            }
           }
         })
         .catch((err) => {
