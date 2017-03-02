d09187a57de11d880008f8945b665f2b1bc5bd1a
Eric Lau <elau002@gmail.com>
2017-03-01 19:01:22 -0800

    Update style to have el inline

--- src/Client/InfoComponents/pokemonInfo/index.vue
+++ src/Client/InfoComponents/pokemonInfo/index.vue
@@ -4,14 +4,14 @@
       <img :src="infoSelect.sprites"/>
       <p> {{ infoSelect.name[0].toUpperCase() + infoSelect.name.slice(1).replace('-', ' ') }}</p>
       <p>Weight: {{infoSelect.weight/10}} kg </p>
-      <div class='abilityTags'>
-        <tr>
-          <td v-for='ability in infoSelect.abilities'> {{ability.ability[0].toUpperCase() + ability.ability.slice(1)}}</td>
-        </tr>
+      <div>
+      <ul class='abilityTags'>
+          <li v-for='ability in infoSelect.abilities'> {{ability.ability[0].toUpperCase() + ability.ability.slice(1).replace('-', ' ') }}</li>
+      </ul>
       </div>
       <table id='pokeStats'>
         <tr>
-          <th  v-for='stat in infoSelect.stats'>{{stat.name[0].toUpperCase() + stat.name.slice(1)}}</th>
+          <th  v-for='stat in infoSelect.stats'>{{stat.name[0].toUpperCase() + stat.name.slice(1).replace('-', ' ')   }}</th>
         </tr>
         <tr>
           <td v-for='stat in infoSelect.stats'>{{stat.value}} </td>
@@ -19,7 +19,7 @@
       </table>
       <ul>
         <li v-for='trait in infoSelect.types.slice().sort((a,b)=>{return a.slot-b.slot})'>
-          <p>{{trait.slot}}: <div class='typeTag' v-bind:id='trait.type'> {{trait.type[0].toUpperCase() + trait.type.slice(1)}} </div> </p>
+          <div class='typeTag' v-bind:id='trait.type'> {{trait.type[0].toUpperCase() + trait.type.slice(1)}} </div>
         </li>
       </ul>
   </div>
@@ -35,8 +35,6 @@
   #pokeStats {
     text-align: center;
     font-size: 12px;
-    font-size: 1.1vw;
-    overflow: auto;
     border: 1px solid #ddd;
     color: black;
     box-shadow: 2px 2px 2px grey;
