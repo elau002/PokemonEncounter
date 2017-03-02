cad9afd1f791197dc8658e2fed5970bfb52357c8
Eric Lau <elau002@gmail.com>
2017-03-01 18:58:14 -0800

    Updated components used in main app

--- src/Client/main.js
+++ src/Client/main.js
@@ -3,12 +3,15 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 
 import router from './Router/router.js';
+import navBar from './Components/NavBarController/index.vue';
 import mixin from './Components/mixinComponent/index.js';
 
 // import App from './app.vue';
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+
+Vue.component('navBar', navBar);
 
 Vue.mixin(mixin);
 
