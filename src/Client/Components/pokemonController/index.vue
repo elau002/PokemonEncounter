bcbee1792b6c2e4a3fb9cba8c64cde85e87e3ace
Eric Lau <elau002@gmail.com>
2017-03-11 11:52:49 -0800

    Change v-on:click to shorthand @click

--- src/Client/Components/pokemonController/index.vue
+++ src/Client/Components/pokemonController/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <button v-on:click.prevent="GenPokemon(1)">Generation 1</button>
-    <button v-on:click.prevent="GenPokemon(2)">Generation 2</button>
-    <button v-on:click.prevent="GenPokemon(3)">Generation 3</button>
-    <button v-on:click.prevent="GenPokemon(4)">Generation 4</button>
-    <button v-on:click.prevent="GenPokemon(5)">Generation 5</button>
-    <button v-on:click.prevent="GenPokemon(6)">Generation 6</button>
+    <button v-if='!showAll' v-on:click.prevent="genPokemon(1)">Generation 1</button>
+    <button v-if='!showAll' v-on:click.prevent="genPokemon(2)">Generation 2</button>
+    <button v-if='!showAll' v-on:click.prevent="genPokemon(3)">Generation 3</button>
+    <button v-if='!showAll' v-on:click.prevent="genPokemon(4)">Generation 4</button>
+    <button v-if='!showAll' v-on:click.prevent="genPokemon(5)">Generation 5</button>
+    <button v-if='!showAll' v-on:click.prevent="genPokemon(6)">Generation 6</button>
     <button v-if='!showAll' v-on:click.prevent="showAll = !showAll">Show All</button>
     <button v-else v-on:click.prevent="showAll = !showAll">Hide All</button>
 
@@ -20,23 +20,7 @@
       </div>
       <div v-else>
         <div v-for='pokemon in pokemons'>
-          <img :src="pokemon.sprites"/>
-          <div>
-            <p>Number: {{pokemon.id}}</p>
-            <p> {{ pokemon.name[0].toUpperCase() + pokemon.name.slice(1) }}</p>
-            <p>Weight: {{pokemon.weight/10}} kg </p>
-            <p v-for='ability in pokemon.abilities'> {{ability}}</p>
-            <ul>
-              <li v-for='stat in pokemon.stats'>
-                <p>{{stat.name}} : {{stat.value}} </p>
-              </li>
-            </ul>
-            <ul>
-              <li v-for='trait in pokemon.types'>
-                <p> {{trait.slot}}: {{trait.type}} </p>
-              </li>
-            </ul>
-          </div>
+          <pokemon-info :info-select='pokemon'></pokemon-info>
         </div>
       </div> 
     </div>
@@ -44,7 +28,13 @@
 </template>
 
 <script>
-module.exports = {
+
+import pokemonInfo from '../../InfoComponents/pokemonInfo/index.vue';
+
+export default {
+  components: {
+    pokemonInfo
+  },
   data: ()=> {
     return {
       pokemon: null,
@@ -62,16 +52,16 @@ module.exports = {
       this.$http.get('http://localhost:4824/api/pokemon/all')
         .then((res)=> {
           this.$data.allGen = res.body;
-          [1,2,3,4,5,6].forEach((number)=> { this.generateGen(number, res.body, this.$data); })
+          [1,2,3,4,5,6].forEach((number)=> { this.generateGen(number, res.body); })
         })
         .catch((err) => {
           console.log(err);
         })
     },
-    genPokemon  (gen) {
+    genPokemon (gen) {
          this.$data.pokemons = this.$data.generations[gen];
     },
-    generateGen  (gen, arr) {
+    generateGen (gen, arr) {
       let temp = [];
       if (gen === 1) {  temp = arr.slice(0, 151) }
       if (gen === 2) {  temp = arr.slice(151, 251) }
