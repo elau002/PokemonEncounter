70d090912489f164ffd67f56e24e8be2b93dbb01
Eric Lau <elau002@gmail.com>
2017-03-24 12:36:43 -0700

    Updated server for deployment

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = require('./router.js');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 4824;
 
 require('./middleware.js')(app, express);
 
+app.use('/', express.static( path.join(__dirname, '../../')));
 app.use('/', router);
 
 app.listen(PORT, function() {
